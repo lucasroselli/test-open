524eb84f0806e59d5301e3fe28868d6bd4eca059
Lucas Roselli <lucasl.roselli@gmail.com>
2016-02-01 13:46:26 -0200

    kjdfls

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ app.get('/profile', function (req, res) {
   res.send('Lucas Leonardi');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
 });
+
